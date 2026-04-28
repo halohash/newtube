ac27576feeaeac963c058d75be7dd34e7f82ce1e
giggityfiles <giggityfiles@gmail.com>
2026-04-27 21:36:54 -0400

    ededeerfrdfrfredfrefdr

--- html5player-vflFQf0-f.js
+++ html5player-vflFQf0-f.js
@@ -5825,9 +5825,9 @@
         this.element = a;
         this.onclick = b;
         this.Yp = d;
-        a.onmousedown = a.onmousedown === undefined
-    ? "xbox"
-    : (bn ? (a.ontouchstart = T(this.vv, this)) : T(this.uv, this));
+        if (a.onmousedown === null) {
+    bn ? a.ontouchstart = T(this.vv, this) : a.onmousedown = T(this.uv, this);
+}
         a.onclick = T(this.$p, this)
     }
     var bn = "ontouchstart"in n
